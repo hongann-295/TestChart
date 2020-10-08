ae33e8e2cfafb10b901987792a460e01d2e19830
an hong <hongan529@gmail.com>
2020-10-08 09:14:57 +0700

    Add project files.

---
+++ TestChart.DAL.Interface/IChartRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestChart.Domain.Request.ChartGender;
+
+namespace TestChart.DAL.Interface
+{
+    public interface IChartRepository
+    {
+        Task<IEnumerable<GetGender>> Gets();
+    }
+}
---
+++ TestChart.DAL/ChartRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestChart.DAL.Interface;
+using TestChart.Domain.Request.ChartGender;
+
+namespace TestChart.DAL
+{
+    public class ChartRepository : IChartRepository
+    {
+        public Task<IEnumerable<GetGender>> Gets()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
